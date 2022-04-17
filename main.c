9fc4fd164660ddf7308d719cc37c5c4b4e55bfa3
Szymon Siemieniuk <szymonsiemieniuk01@gmail.com>
2022-04-17 19:54:35 +0200

    Fixed conditions; removed file open

--- main.c
+++ main.c
@@ -5,34 +5,26 @@
 
 int main(int argc, char *argv[])
 {
-    if(argc < 3)
+    // check if there is correct number of parameters
+    if (!(argc >= 3 && argc <= 4))
     {
         printf("Invalid number of arguments\n");
         exit(1);
     }
 
-    FILE* fp = fopen(argv[1], "r");
-    if (!fp) {
-        printf("File %s not found", argv[1]);
-        exit(1);
-    }
-    fclose(fp);
-    
-    
     struct Content cont = parse_ini_file(argv[1]);
-
-    if (strcmp(argv[2], "expression") == 0)
+    // check if it is "expression" case or not
+    if (argc == 4 && strcmp(argv[2], "expression") == 0)
     {
         run_expression(&cont, argv[3]);
     }
     else
     {
         char* val = get_value(&cont, argv[2]);
-        if(val == NULL)
+        if (val == NULL)
             exit(1);
         printf("%s\n", val);
     }
-
     clear_content(&cont);
 
     return 0;
